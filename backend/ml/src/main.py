0ad42d5b5fd33396de5af367e00a287fe4776a3f
themahmoudzaki <Mahmoudzakylinkedin@gmail.com>
2025-09-22 01:27:19 +0200

    feat: Implemented Data Loader & Merger

--- backend/ml/src/main.py
+++ backend/ml/src/main.py
@@ -2,20 +2,33 @@ import os
 from dotenv import load_dotenv
 from data.data_loader_and_merger import ExoPlanetData
 from data.data_visualizer import EXODataVisualizer
+
 load_dotenv()
 
 
 def main():
-    k2_path = os.getenv("K2_DATA_PATH", "ml/K2_mission_captured_data.csv")
-    kepler_path = os.getenv("KEPLER_DATA_PATH", "ml/kepler_captured_data.csv")
-    tess_path = os.getenv("TESS_DATA_PATH", "ml/TESS_mission_captured_data.csv")
+    #
+    # Docker to be implemented
+    #
+    # k2_path = os.getenv("K2_DATA_PATH", "ml/data/K2_mission_captured_data.csv")
+    # kepler_path = os.getenv("KEPLER_DATA_PATH", "ml/data/kepler_captured_data.csv")
+    # tess_path = os.getenv("TESS_DATA_PATH", "ml/data/TESS_mission_captured_data.csv")
+
+    from pathlib import Path
+
+    data_folder = Path("..") / "data"
+    k2_path = data_folder / "K2_mission_captured_data.csv"
+    kepler_path = data_folder / "kepler_captured_data.csv"
+    tess_path = data_folder / "TESS_mission_captured_data.csv"
 
     data_loader = ExoPlanetData(
-        kepler_path=kepler_path, k2_path=k2_path, tess_path=tess_path
+        kepler_path=kepler_path,
+        k2_path=k2_path,
+        tess_path=tess_path,
+        data_folder=data_folder,
     )
     df = data_loader.merge_data()
     data_visualizer = EXODataVisualizer(df)
-    
 
 
 if __name__ == "__main__":
